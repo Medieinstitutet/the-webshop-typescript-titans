2ee05af38230dcc4a945a5edcf1cc58efc66f8e4
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-01-02 23:07:29 +0100

    added ts to change parent classes for searchform

---
+++ src/ts/navbar/navbar.ts
@@ -0,0 +1,21 @@
+const inputGroup: HTMLElement | null = document.querySelector('.input-group');
+const nav: HTMLElement | null = document.querySelector('nav');
+const mobileSearchBarContainer: HTMLElement | null = document.querySelector('.mobile-searchbar-container');
+
+function handleResize() {
+  const isMobileView: boolean = window.innerWidth <= 1024; 
+
+  if (isMobileView) {
+    if (mobileSearchBarContainer && inputGroup) {
+      mobileSearchBarContainer.appendChild(inputGroup);
+    }
+  } else {
+    if (nav && inputGroup) {
+      nav.appendChild(inputGroup);
+    }
+  }
+}
+
+handleResize();
+
+window.addEventListener('resize', handleResize);
