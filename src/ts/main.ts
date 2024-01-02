7e4113e50898d21adcfd1d5195e0a75b743d6be3
Fika03 <filipkarlstrom03@gmail.com>
2024-01-02 14:48:59 +0100

    added Product class and loop to create HTMl

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,5 @@
 import "./../scss/style.scss";
+import { displayProducts } from "./productPage/displayProducts";
 import "./productPage/productPage";
+
+displayProducts();
---
+++ src/ts/productPage/productPage.ts
@@ -0,0 +1,19 @@
+import { Product } from "../../models/Product";
+
+const testProduct1: Product = new Product(
+  "Old School Runescape",
+  199,
+  "MMORPG"
+);
+
+const testProduct2: Product = new Product(
+  "Old School Runescape",
+  199,
+  "MMORPG"
+);
+const testProduct3: Product = new Product(
+  "Old School Runescape",
+  199,
+  "MMORPG"
+);
+export let productList = [testProduct1, testProduct2, testProduct3];
