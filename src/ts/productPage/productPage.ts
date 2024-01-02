e6a1429e7a8fe429c60a2d0f96e6cdc78cb42da0
Fika03 <filipkarlstrom03@gmail.com>
2024-01-02 15:05:38 +0100

    small fixes

--- src/ts/productPage/productPage.ts
+++ src/ts/productPage/productPage.ts
@@ -1,4 +1,4 @@
-import { Product } from "../../models/Product";
+import { Product } from "../models/Product";
 
 const testProduct1: Product = new Product(
   "Old School Runescape",
@@ -6,14 +6,7 @@ const testProduct1: Product = new Product(
   "MMORPG"
 );
 
-const testProduct2: Product = new Product(
-  "Old School Runescape",
-  199,
-  "MMORPG"
-);
-const testProduct3: Product = new Product(
-  "Old School Runescape",
-  199,
-  "MMORPG"
-);
+const testProduct2: Product = new Product("World of Warcraft", 399, "MMORPG");
+const testProduct3: Product = new Product("Minecraft", 150, "Adventure");
+
 export let productList = [testProduct1, testProduct2, testProduct3];
