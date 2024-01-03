fad1e907e145dda0d4ce05d2f8313df8fbe4df57
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-01-03 20:57:19 +0100

    navbar hamburger menu and navlist

--- src/ts/navbar/navbar.ts
+++ src/ts/navbar/navbar.ts
@@ -1,20 +1,48 @@
-const inputGroup: HTMLElement | null = document.querySelector('.input-group');
-const nav: HTMLElement | null = document.querySelector('nav');
-const mobileSearchBarContainer: HTMLElement | null = document.querySelector('.mobile-searchbar-container');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerIcon: HTMLElement | null = document.querySelector('#nav-hamburger-icon');
+  const navList: HTMLElement | null = document.querySelector('.nav-list');
+  const mobileSearchBarContainer: HTMLElement | null = document.querySelector('.mobile-searchbar-container');
+  const inputGroup: HTMLElement | null = document.querySelector('.input-group');
+  const nav: HTMLElement | null = document.querySelector('nav');
+  let isNavListVisible: boolean = false;
+  let isMobileView: boolean = window.innerWidth <= 1024;
 
-function handleResize() {
-  const isMobileView: boolean = window.innerWidth <= 1024;
+  const updateNavListVisibility = () => {
+      navList?.classList.toggle('visible', isNavListVisible && isMobileView);
+      navList?.classList.toggle('hidden', !isNavListVisible || !isMobileView);
+      updateMobileSearchBarPosition();
+  };
 
-  if (isMobileView) {
-    if (mobileSearchBarContainer && inputGroup) {
-      mobileSearchBarContainer.appendChild(inputGroup);
-    }
-  } else {
-    if (nav && inputGroup) {
-      nav.appendChild(inputGroup);
-    }
-  }
-}
-handleResize();
-window.addEventListener('resize', handleResize);
+  const updateMobileSearchBarPosition = () => {
+      if (isMobileView) {
+          mobileSearchBarContainer?.classList.toggle('moved-down', isNavListVisible);
+          mobileSearchBarContainer?.classList.toggle('moved-up', !isNavListVisible);
+      } else {
+          mobileSearchBarContainer?.classList.remove('moved-down', 'moved-up');
+      }
+  };
 
+  const toggleNavList = () => {
+      isNavListVisible = !isNavListVisible;
+      updateNavListVisibility();
+  };
+
+  const handleResize = () => {
+      const newIsMobileView: boolean = window.innerWidth <= 1024;
+      if (newIsMobileView !== isMobileView) {
+          isMobileView = newIsMobileView;
+          updateNavListVisibility();
+      }
+
+      if (isMobileView && mobileSearchBarContainer && inputGroup) {
+          mobileSearchBarContainer.appendChild(inputGroup);
+      } else if (nav && inputGroup) {
+          nav.appendChild(inputGroup);
+      }
+  };
+
+  hamburgerIcon?.addEventListener('click', toggleNavList);
+  window.addEventListener('resize', handleResize);
+  handleResize();
+  updateNavListVisibility();
+});
