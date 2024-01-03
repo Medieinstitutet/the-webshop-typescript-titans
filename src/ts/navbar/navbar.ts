9930615d1ab6056fe7f27c51cae2b9fdbc735c30
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-01-04 00:05:24 +0100

    fixed hamburger icon when nav-list is visible

--- src/ts/navbar/navbar.ts
+++ src/ts/navbar/navbar.ts
@@ -1,48 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerIcon: HTMLElement | null = document.querySelector('#nav-hamburger-icon');
-  const navList: HTMLElement | null = document.querySelector('.nav-list');
-  const mobileSearchBarContainer: HTMLElement | null = document.querySelector('.mobile-searchbar-container');
-  const inputGroup: HTMLElement | null = document.querySelector('.input-group');
-  const nav: HTMLElement | null = document.querySelector('nav');
-  let isNavListVisible: boolean = false;
-  let isMobileView: boolean = window.innerWidth <= 1024;
+    
+   
+    const navList = document.querySelector('.nav-list') as HTMLElement;
+    const mobileSearchBarContainer = document.querySelector('.mobile-searchbar-container') as HTMLElement;
+    const inputGroup = document.querySelector('.input-group') as HTMLElement;
+    const nav = document.querySelector('nav') as HTMLElement;
+    const hamburgerIcon = document.getElementById('nav-hamburger-icon') as HTMLElement;
+    const xIcon = document.getElementById('nav-x-icon') as HTMLElement;
+    let isNavListVisible: boolean = false;
+    let isMobileView: boolean = window.innerWidth <= 1024;
 
-  const updateNavListVisibility = () => {
-      navList?.classList.toggle('visible', isNavListVisible && isMobileView);
-      navList?.classList.toggle('hidden', !isNavListVisible || !isMobileView);
+
+
+    const updateNavListVisibility = (): void => {
+      navList.classList.toggle('visible', isNavListVisible && isMobileView);
+      navList.classList.toggle('hidden', !isNavListVisible || !isMobileView);
+
       updateMobileSearchBarPosition();
-  };
+      updateIcons();
+    };
 
-  const updateMobileSearchBarPosition = () => {
+    const updateMobileSearchBarPosition = (): void => {
       if (isMobileView) {
-          mobileSearchBarContainer?.classList.toggle('moved-down', isNavListVisible);
-          mobileSearchBarContainer?.classList.toggle('moved-up', !isNavListVisible);
+        mobileSearchBarContainer.classList.toggle('moved-down', isNavListVisible);
+        mobileSearchBarContainer.classList.toggle('moved-up', !isNavListVisible);
       } else {
-          mobileSearchBarContainer?.classList.remove('moved-down', 'moved-up');
+        mobileSearchBarContainer.classList.remove('moved-down', 'moved-up');
       }
-  };
+    };
 
-  const toggleNavList = () => {
+    const updateIcons = (): void => {
+        if (isMobileView) {
+            if (isNavListVisible) {
+                hamburgerIcon.classList.add('hide-icon');
+                hamburgerIcon.classList.remove('show-icon');
+                xIcon.classList.add('show-icon');
+                xIcon.classList.remove('hide-icon');
+            } else {
+                hamburgerIcon.classList.add('show-icon');
+                hamburgerIcon.classList.remove('hide-icon');
+                xIcon.classList.add('hide-icon');
+                xIcon.classList.remove('show-icon');
+            }
+        } else {
+            hamburgerIcon.classList.add('hide-icon');
+            xIcon.classList.add('hide-icon');
+        }
+    };
+
+
+    const toggleNavList = (): void => {
       isNavListVisible = !isNavListVisible;
       updateNavListVisibility();
-  };
+    };
 
-  const handleResize = () => {
+    const handleResize = (): void => {
       const newIsMobileView: boolean = window.innerWidth <= 1024;
       if (newIsMobileView !== isMobileView) {
-          isMobileView = newIsMobileView;
-          updateNavListVisibility();
+        isMobileView = newIsMobileView;
+        updateNavListVisibility();
       }
 
       if (isMobileView && mobileSearchBarContainer && inputGroup) {
-          mobileSearchBarContainer.appendChild(inputGroup);
+        mobileSearchBarContainer.appendChild(inputGroup);
       } else if (nav && inputGroup) {
-          nav.appendChild(inputGroup);
+        nav.appendChild(inputGroup);
       }
-  };
+    };
 
-  hamburgerIcon?.addEventListener('click', toggleNavList);
-  window.addEventListener('resize', handleResize);
-  handleResize();
-  updateNavListVisibility();
+
+    window.addEventListener('resize', handleResize);
+    hamburgerIcon.addEventListener('click', toggleNavList);
+    xIcon.addEventListener('click', toggleNavList);
+    handleResize();
+    updateNavListVisibility();
 });
