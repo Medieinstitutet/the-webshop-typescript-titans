a8b35e815db2ea80f956d80e21aae837c424bb73
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-01-04 17:56:25 +0100

    changes on cart

--- src/ts/checkout/checkout.ts
+++ src/ts/checkout/checkout.ts
@@ -1,6 +1,9 @@
 const navBagIcon = document.getElementById('nav-bag-icon') as HTMLElement;
 const checkout = document.getElementById('checkout') as HTMLElement;
 const closeCartIcon = document.getElementById('closeCartIcon') as HTMLElement;
+const goToPayment = document.getElementById('goToPayment') as HTMLElement;
+
+
 
 navBagIcon.addEventListener('click', function() {
     checkout.style.display = 'flex';
@@ -9,3 +12,11 @@ navBagIcon.addEventListener('click', function() {
 closeCartIcon.addEventListener('click', function() {
     checkout.style.display = 'none';
 });
+
+
+
+
+goToPayment.addEventListener('click', function() {
+    // Byt ut "ny-sida.html" med den faktiska sidan du vill navigera till.
+    window.location.href = 'payment.html';
+})
