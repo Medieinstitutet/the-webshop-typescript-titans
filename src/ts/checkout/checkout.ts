6ac66fdd3d985412e0157c547bf7adc2ac3972d6
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-01-06 20:32:22 +0100

    icon changes

--- src/ts/checkout/checkout.ts
+++ src/ts/checkout/checkout.ts
@@ -14,9 +14,30 @@ closeCartIcon.addEventListener('click', function() {
 });
 
 
-
+//Funktion som gör att man skickat vidare till betalningen
 
 goToPayment.addEventListener('click', function() {
    
     window.location.href = 'order.html';
 })
+
+
+// Antal i varukorgen
+let cartCount: number = 0;
+
+// Funktion för att uppdatera antalet och visa/dölja det
+function updateCartCount(count: number): void {
+  cartCount = count;
+  const cartCountSpan: HTMLElement | null = document.getElementById('cart-count');
+
+  if (cartCountSpan) {
+    // Uppdatera antalet
+    cartCountSpan.textContent = cartCount.toString();
+
+    // Visa/dölj antalspännet baserat på om varukorgen är tom eller inte
+    cartCountSpan.style.display = cartCount > 0 ? 'block' : 'none';
+  }
+}
+
+
+updateCartCount(1)
