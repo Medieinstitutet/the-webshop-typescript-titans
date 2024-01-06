165585aa9e36da3d49b907ca68dce6c31039998d
Marcus Gustavsson <mackan1892@outlook.com>
2024-01-06 20:20:25 +0100

    Added Html and scss to payment

---
+++ src/ts/payment/payment.ts
@@ -0,0 +1,16 @@
+function validateForm() {
+    // Hämta värdet från input-fältet med id "card-holder-name"
+    let x = (document.getElementById("card-holder-name") as HTMLInputElement).value;
+   
+ 
+    if (x === "") {
+      alert("Kortinnehavarens uppgifter måste fyllas i");
+      return false;
+    }
+ 
+ 
+    return true;
+  }
+ 
+  // Knyt funktionen till ett fält på fönstret så att den kan nås globalt
+  (window as any).validateForm = validateForm;
