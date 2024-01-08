271c16c36ae3df5102b8eca646452e5a5818b76c
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-01-08 17:12:15 +0100

    code comments

--- src/ts/productPage/addRandomPrices.ts
+++ src/ts/productPage/addRandomPrices.ts
@@ -3,15 +3,16 @@ import { Games } from '../../models/Games'
 
 interface GamesWithPrice extends Games {
     price: number;
-  }
+  }//Förlänger interfacet med ny egenskap och nytt namn så att pris går att lägga till i varje objekt
   
   export async function addRandomPrices(): Promise<GamesWithPrice[]> {
-    
+    //Om det finns spel från tidigare session så skickar functionen dessa
     const storedGames = localStorage.getItem('gamesWithPrices');
     if (storedGames) {
       return JSON.parse(storedGames) as GamesWithPrice[];
     }
-  
+    //Om det inte finns spel lagrade i localstorage så kommer spelen att hämtas från api:et och ett randompris läggs in i varje spelobject, 
+    //varje spel läggs in i det nya interfacet med pris. 
     try {
       const games = await getAllGames();
   
@@ -20,10 +21,13 @@ interface GamesWithPrice extends Games {
 
         price: Math.floor(Math.random() * (60 - 10 + 1)) + 10
       }));
+      
   
       localStorage.setItem('gamesWithPrices', JSON.stringify(gamesWithPrice));
   
       return gamesWithPrice;
+      //Spel med pris läggs in i localstorage och funktionen returnerar spel med priser
+
     } catch (error) {
       console.error("Error adding prices to games:", error);
       throw error;
